2786924f88f57013950d835231b87c9faa29a1b2
kei <onkou2014@gmail.com>
2022-05-22 14:14:42 +0900

    スタート画面、ロビー選択画面追加

---
+++ src/SelectHostOrJoinSceneController.java
@@ -0,0 +1,37 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SelectHostOrJoinSceneController {
+
+    @FXML
+    private Button createLobbyButton;
+
+    @FXML
+    private Button joinLobbyButton;
+
+    @FXML
+    private Button backButton;
+
+    private IDaifugoApp callback;
+
+    public void SetCallback(IDaifugoApp callback) {
+        this.callback = callback;
+    }
+
+    @FXML
+    void onPressedBackButton(ActionEvent event) {
+        callback.ShowStartScene();
+    }
+
+    @FXML
+    void onPressedCreateLobbyButton(ActionEvent event) {
+        callback.ShowCreateLobbyScene();
+    }
+
+    @FXML
+    void onPressedJoinLobbyButton(ActionEvent event) {
+
+    }
+
+}
---
+++ src/StartSceneController.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StartSceneController {
+
+    @FXML
+    private TextField playerNameTextField;
+
+    @FXML
+    private Button startButton;
+
+    private IServerConnectable connection;
+    private IDaifugoApp app;
+
+    public void SetCallback(IServerConnectable connection, IDaifugoApp app) {
+        this.connection = connection;
+        this.app = app;
+    }
+
+    @FXML
+    void onPressedStartButton(ActionEvent event) {
+        connection.RequestCreateAccount(playerNameTextField.getText());
+        app.SetName(playerNameTextField.getText());
+    }
+}
+
